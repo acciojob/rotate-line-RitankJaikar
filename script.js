1337ec5b4d2e428da1b076f1454112a0b9e6a6cc
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-22 20:27:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const line = document.querySelector("#line");
+let i=0;
+
+setInterval(() => {
+	line.style.transform= `rotate(${i}deg)`;
+	i+=2;
+	if(i===360) {
+		i=0;
+	}
+	//console.log(i);
+}, 20);
